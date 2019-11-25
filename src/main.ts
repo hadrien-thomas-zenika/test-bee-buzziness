20592963edfb0f407df86a02d439357afb429d04
unknown <hadrien.thomas@zenika.com>
2019-11-25 19:43:16 +0100

    feature(conversion): allow passing arguments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { app } from "./app";
+import { ConsoleDisplayConversionService } from "./services/consoleDisplayConversionService";
+import { LcdConversionService } from "./services/lcdConversionService";
+import { LcdDigit } from "./services/lcdDigit";
 
-const port: number = 3000;
-app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
-});
+const lcdConversionService: LcdConversionService = new LcdConversionService();
+const consoleDisplayConversionService: ConsoleDisplayConversionService = new ConsoleDisplayConversionService();
+
+const numberAsString: string = process.argv[2];
+
+const lcdDigits: LcdDigit[] = lcdConversionService.convert(
+	numberAsString
+		.split("")
+		.map((digitAsString: string) => parseInt(digitAsString, 10)),
+);
+
+console.log(
+	consoleDisplayConversionService.display(
+		lcdDigits,
+	),
+);
